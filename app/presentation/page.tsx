984967dd7d39bc0d1eb09c8190148ce644756c58
Elsa Catoire <elsa.catoire@gmail.com>
2023-07-25 15:13:21 +0200

    ajout de la page présentation

---
+++ app/presentation/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+    return (
+        <main className="px-6 mx-auto">
+            <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+                En savoir un petit peu plus sur moi 👋&nbsp;
+                <span className="whitespace-nowrap">
+                    I&apos;m <span className="font-bold">Elsa</span>.
+                </span>
+            </p>
+
+        </main>
+    )
+}
