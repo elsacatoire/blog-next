abb798b2dc597348291adb75074fe388c32b0d49
Elsa Catoire <elsa.catoire@gmail.com>
2023-07-25 14:58:30 +0200

    footer ok

---
+++ app/components/Footer.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import { FaGithub, FaLinkedin, FaMedium, FaInstagram } from "react-icons/fa"
+
+export default function Footer() {
+    return (
+
+        <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023.
+                    <Link href="/" className="hover:underline">Elsa Catoire</Link>. All Rights Reserved.
+                </span>
+                <ul className="flex flex-wrap items-center mt-8 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                    <li>
+                        <Link href="https://www.linkedin.com/in/elsa-catoire-11402753/" className="mr-5 hover:underline md:mr-10" target="_blank" rel="noopener noreferrer">
+                            <FaLinkedin className="w-10 h-10 m-2" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="https://github.com/elsacatoire" className="mr-5 hover:underline md:mr-10" target="_blank" rel="noopener noreferrer">
+                            <FaGithub className="w-10 h-10 m-2" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="https://www.instagram.com/elsa_des_bois/" className="mr-5 hover:underline md:mr-10" target="_blank" rel="noopener noreferrer">
+                            <FaInstagram className="w-10 h-10 m-2" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="https://medium.com/@elsa.catoire" className="mr-5 hover:underline md:mr-10" target="_blank" rel="noopener noreferrer">
+                            <FaMedium className="w-10 h-10 m-2" />
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
