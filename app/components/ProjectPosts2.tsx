58cb35be895f6bd936987a8ae48fdd67aa503234
Elsa Catoire <elsa.catoire@gmail.com>
2023-07-26 23:15:43 +0200

    card for projects

--- app/components/ProjectPosts2.tsx
+++ app/components/ProjectPosts2.tsx
@@ -6,14 +6,14 @@ export default function ProjectPosts2() {
     const posts = getSortedProjectsData().slice(-2)
     return (
         <section className="mt-6 mx-auto max-w-2xl">
-            <h2 className="text-2xl font-bold
-             dark:text-white/90">Deux derniers projets</h2>
+            <h2 className="text-2xl font-bold text-center text-green-400
+             dark:text-white/90">Mes deux derniers projets</h2>
             <ul className="w-full mb-3">
                 {posts.map(post => (
                     <ListItemProject key={post.id} post={post} />
                 ))}
             </ul>
-            <Link className="underline text-green-700" href="/projets">Voir tous les projets</Link>
+            <Link className="underline text-green-400" href="/projets">Voir tous les projets</Link>
 
         </section>
     )
