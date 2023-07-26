79ea8fadda4722e48c4db3494d336e2ba301eb5b
Elsa Catoire <elsa.catoire@gmail.com>
2023-07-26 22:47:17 +0200

    bugfix anonymus funtion export

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,6 +1,6 @@
 import Posts from "../components/Posts"
 
-export default function posts() {
+export default function PostsPage() {
     return (
         <main className="px-6 mx-auto">
             <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
