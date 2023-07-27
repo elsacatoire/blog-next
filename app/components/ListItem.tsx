ac70cae38b4b10d428e7e8102d160a2656424ef6
Elsa Catoire <elsa.catoire@gmail.com>
2023-07-27 22:11:04 +0200

    blog ok dispo page

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -12,8 +12,8 @@ export default function ListItem({ post }: Props) {
 
     return (
 
-        <Link className="mx-auto w-[80%] flex items-center bg-gray-200 border border-green-700 shadow md:flex-row md:max-w-xl hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700" href={`/posts/${id}`}>
-            <Image className="imgProject" src={image} alt="" width={192} height={192} />
+        <Link className="mt-5 flex flex-col items-center rounded-lg bg-gray-200 border border-green-700 shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700" href={`/posts/${id}`}>
+            <Image className="imgProject rounded-l" src={image} alt="" width={192} height={192} />
             <div className="flex flex-col justify-between p-4 leading-normal">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
                 <p className="mb-3 font-normal text-green-700 dark:text-gray-400">{formattedDate}</p>
