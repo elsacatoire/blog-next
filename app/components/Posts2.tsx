eaaceb5bcce7402c76daa5ab04092f57995af7ea
Elsa Catoire <elsa.catoire@gmail.com>
2023-07-27 23:08:08 +0200

    fix card accueil

--- app/components/Posts2.tsx
+++ app/components/Posts2.tsx
@@ -13,7 +13,7 @@ export default function Posts2() {
                     <ListItem key={post.id} post={post} />
                 ))}
             </ul>
-            <Link className="underline text-green-400" href="/posts">Voir tous les articles</Link>
+            <Link className="underline text-green-400" href="/posts">Les articles &rarr;</Link>
         </section>
     )
 }
