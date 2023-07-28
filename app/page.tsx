62479a5e2d1adf99d0c52642383df179d2d25552
Elsa Catoire <elsa.catoire@gmail.com>
2023-07-28 20:20:27 +0200

    fix xrap des cards skill tech

--- app/page.tsx
+++ app/page.tsx
@@ -35,26 +35,33 @@ export default function Home() {
           </div>
 
         </div>
-        <div className="justify-center flex felx-wrap mt-12">
-          <div className="block w-full md:w-1/3 md:order-1 mr-3 max-w-sm p-6 bg-gray-300 border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Technos &#9829;</h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; JavaScript/HYML/CSS</p>
-            <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; TypeScript, Next.js</p>
-            <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; Python</p>
-            <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; NoSQL avec MongoDB</p>
-            <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; API REST</p>
+        <div className="justify-center flex flex-wrap mt-12">
+          <div className="w-full md:w-1/3 md:order-1 p-3">
+            <div className="h-full max-w-sm p-6 bg-gray-300 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Technos &#9829;</h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; JavaScript/HYML/CSS</p>
+              <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; TypeScript, Next.js</p>
+              <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; Python</p>
+              <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; NoSQL avec MongoDB</p>
+              <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; API REST</p>
+            </div>
           </div>
-          <div className="block w-full md:w-1/3 md:order-2 mr-3 max-w-sm p-6 bg-gray-300 border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">J&rsquo;apprends aussi</h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; Java</p>
-            <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; PHP</p>
-            <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; SQL</p>
+          <div className="w-full md:w-1/3 md:order-1 p-3">
+            <div className="h-full max-w-sm p-6 bg-gray-300 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">J&rsquo;apprends aussi</h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; Java</p>
+              <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; PHP</p>
+              <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; SQL</p>
+            </div>
           </div>
-          <div className="block w-full md:w-1/3 md:order-3 max-w-sm p-6 bg-gray-300 border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Backlog</h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; Django, FAST API</p>
-            <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; Dev mobile</p>
-            <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; Docker</p>
+
+          <div className="w-full md:w-1/3 md:order-1 p-3">
+            <div className="h-full max-w-sm p-6 bg-gray-300 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Backlog</h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; Django, FAST API</p>
+              <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; Dev mobile</p>
+              <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; Docker</p>
+            </div>
           </div>
         </div>
 
