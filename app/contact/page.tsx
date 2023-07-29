9d42295f8c96cc7f91d61d07fc0ccf53083c7d47
Elsa Catoire <elsa.catoire@gmail.com>
2023-07-29 12:14:22 +0200

    contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,25 +6,34 @@ export default function Contact() {
         <main>
             <div className="text-lg text-left block p-10 text-gray-900 bg-gray-200 border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                 <div className="flex flex-col-reverse md:flex-row md:space-x-8 items-start">
-                    <div className="md:w-1/2 md:order-1 ml-5">
+                    <div className="md:w-2/3 md:order-1 ml-5">
                         <h5 className="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                             Curieuse et ouverte la conversation, n&rsquo;hésitez pas à me contacter
                         </h5>
                         <br />
                         <p className="font-normal text-gray-700 dark:text-gray-400">
-
                         </p>
                         <br />
+                        <section className="text-center">
+                            <h5 className="mt-5 text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                Des feedbacks techniques, typo, design ?
+                            </h5>
+                            <p className="font-normal text-gray-700 dark:text-gray-400">J'ai réalisé ce site pour apprendre</p>
+                            <p className="font-normal text-gray-700 dark:text-gray-400">Il est très certainement imparfait</p>
+                            <p className="font-normal text-gray-700 dark:text-gray-400">Tout retour bienveillant m'aiderait à progresser</p>
+                            <h5 className="text-center mb-2 text-2xl font-bold tracking-tight text-green-700 dark:text-white">
+                                &#x2794; <Link href="https://github.com/elsacatoire/next-portfolio/issues">GitHub Issues</Link>
+                            </h5></section>
 
                     </div>
 
-                    <div className="md:w-1/2 md:order-2 rounded-lg center">
+                    <div className=" md:w-1/3 md:order-2 rounded-lg center">
                         <Link href="https://www.polarsteps.com/ElsaCatoire">
                             <Image
                                 className="mx-auto mt-0 rounded-lg"
                                 src="/images/chat.jpg"
-                                width={1000}
-                                height={1000}
+                                width={400}
+                                height={400}
                                 alt="Elsa Catoire"
                                 priority={true}
                             />
@@ -32,7 +41,7 @@ export default function Contact() {
                     </div>
 
                 </div>
-                <div className="justify-center flex flex-wrap mt-12">
+                <div className="justify-center flex flex-wrap mt-5">
                     <div className="w-full md:w-1/3 md:order-1 p-3">
                         <div className="h-full max-w-sm p-6 bg-gray-300 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Coordonnées</h5>
@@ -67,16 +76,7 @@ export default function Contact() {
                         </div>
                     </div>
                 </div>
-                <section className="text-center">
-                    <h5 className="mt-20 text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Des feedbacks techniques, typo, design ?
-                    </h5>
-                    <p className="font-normal text-gray-700 dark:text-gray-400">J'ai réalisé ce site pour apprendre</p>
-                    <p className="font-normal text-gray-700 dark:text-gray-400">Il est très certainement imparfait</p>
-                    <p className="font-normal text-gray-700 dark:text-gray-400">Tout retour bienveillant m'aiderait à progresser</p>
-                    <h5 className="text-center mb-2 text-2xl font-bold tracking-tight text-green-700 dark:text-white">
-                        &#x2794; <Link href="https://github.com/elsacatoire/next-portfolio/issues">GitHub Issues</Link>
-                    </h5></section>
+
 
             </div>
         </main>
