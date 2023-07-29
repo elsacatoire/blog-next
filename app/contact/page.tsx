2b8d55233f6fc63753b57c08eff678fbf6b17faf
Elsa Catoire <elsa.catoire@gmail.com>
2023-07-29 11:56:28 +0200

    refont contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,31 +1,62 @@
 import Link from "next/link"
+import Image from "next/image"
 
 export default function Contact() {
     return (
         <main>
-            <div className="p-20 text-lg text-left block max-w-m text-gray-900 bg-gray-200 border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <div className="text-lg text-left block p-10 text-gray-900 bg-gray-200 border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <div className="flex flex-col-reverse md:flex-row md:space-x-8 items-start">
+                    <div className="md:w-1/2 md:order-1 ml-5">
+                        <h5 className="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            Curieuse et ouverte la conversation, n&rsquo;hésitez pas à me contacter
+                        </h5>
+                        <br />
+                        <p className="font-normal text-gray-700 dark:text-gray-400">
 
-                <h5 className="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    Curieuse et ouverte la conversation, n&rsquo;hésitez pas à me contacter
-                </h5>
-                <br />
-                <div className="justify-center flex felx-wrap mt-12">
-                    <div className="block w-full md:w-1/3 md:order-1 mr-3 max-w-sm p-6 bg-gray-300 border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Coordonnées</h5>
-                        <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; +336 62 04 88 85</p>
-                        <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; elsa.catoire@gmail.com</p>
-                    </div>
-                    <div className="block w-full md:w-1/3 md:order-2 mr-3 max-w-sm p-6 bg-gray-300 border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Réseaux profesionnels</h5>
-                        <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; LinkedIn</p>
-                        <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; GitHub</p>
-                    </div>
-                    <div className="block w-full md:w-1/3 md:order-3 max-w-sm p-6 bg-gray-300 border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Autres</h5>
-                        <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; Medium</p>
-                        <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; Instagram</p>
+                        </p>
+                        <br />
+
                     </div>
 
+                    <div className="md:w-1/2 md:order-2 rounded-lg center">
+                        <Link href="https://www.polarsteps.com/ElsaCatoire">
+                            <Image
+                                className="mx-auto mt-0 rounded-lg"
+                                src="/images/polarstep.jpg"
+                                width={1000}
+                                height={1000}
+                                alt="Elsa Catoire"
+                                priority={true}
+                            />
+                        </Link>
+                    </div>
+
+                </div>
+                <div className="justify-center flex flex-wrap mt-12">
+                    <div className="w-full md:w-1/3 md:order-1 p-3">
+                        <div className="h-full max-w-sm p-6 bg-gray-300 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Coordonnées</h5>
+                            <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; +336 62 04 88 85</p>
+                            <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; elsa.catoire@gmail.com</p>
+
+                        </div>
+                    </div>
+                    <div className="w-full md:w-1/3 md:order-2 p-3">
+                        <div className="h-full max-w-sm p-6 bg-gray-300 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Réseaux profesionnels</h5>
+                            <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; LinkedIn</p>
+                            <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; GitHub</p>
+                        </div>
+                    </div>
+
+                    <div className="w-full md:w-1/3 md:order-3 p-3">
+                        <div className="h-full max-w-sm p-6 bg-gray-300 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Ecrits, voyages</h5>
+                            <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; Medium</p>
+                            <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; Polarstep</p>
+                            <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; Instagram</p>
+                        </div>
+                    </div>
                 </div>
                 <h5 className="mt-10 text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                     Des feedbacks techniques, typo, design ?
@@ -34,10 +65,6 @@ export default function Contact() {
                     &#x2794; <Link href="https://github.com/elsacatoire/next-portfolio/issues">GitHub Issues</Link>
                 </h5>
             </div>
-
-
-
-
         </main>
     )
 }
