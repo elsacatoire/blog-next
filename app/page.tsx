66c425a01e6829760e020aa5321e5dd59261e15b
Elsa Catoire <elsa.catoire@gmail.com>
2023-07-29 12:21:00 +0200

    fix mots trop longs

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
           <div className="w-full md:w-1/3 md:order-1 p-3">
             <div className="h-full max-w-sm p-6 bg-gray-300 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Technos &#9829;</h5>
-              <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; JavaScript/HYML/CSS</p>
+              <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; JavaScript, HYML, CSS</p>
               <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; TypeScript, Next.js</p>
               <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; Python</p>
               <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; NoSQL avec MongoDB</p>
