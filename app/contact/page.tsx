fddbc44e8b7a820f1b1ca07bddcc6a5e4dbfda87
Elsa Catoire <elsa.catoire@gmail.com>
2023-07-29 12:21:53 +0200

    espace contacts

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -46,6 +46,7 @@ export default function Contact() {
                         <div className="h-full max-w-sm p-6 bg-gray-300 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Coordonnées</h5>
                             <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; +336 62 04 88 85</p>
+                            <br />
                             <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; elsa.catoire [at]gmail.com</p>
                         </div>
                     </div>
