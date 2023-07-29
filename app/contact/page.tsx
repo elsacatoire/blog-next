280d3d0db333efb445e433bda15049f79d08130a
Elsa Catoire <elsa.catoire@gmail.com>
2023-07-29 12:22:17 +0200

    fixmail

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -47,7 +47,7 @@ export default function Contact() {
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Coordonnées</h5>
                             <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; +336 62 04 88 85</p>
                             <br />
-                            <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; elsa.catoire [at]gmail.com</p>
+                            <p className="font-normal text-gray-700 dark:text-gray-400">&#x2022; elsa.catoire @gmail.com</p>
                         </div>
                     </div>
                     <div className="w-full md:w-1/3 md:order-2 p-3">
