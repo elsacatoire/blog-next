c8f89a0f7cb6482ddbb6d6ad5eea7de4605b3fe6
Elsa Catoire <elsa.catoire@gmail.com>
2023-08-04 13:49:39 +0200

    small fix

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,12 +8,12 @@ export default function Navbar() {
         <nav className="bg-gray-200 border-gray-200 dark:bg-gray-900 p-4 sticky top-0 drop-shadow-xl z-10">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <Link href="/" className="flex items-center">
-                    <Image src="/images/logo.png" className="h-12 w-12 mr-8" alt="Flowbite Logo" width={100} height={100} />
+                    <Image src="/images/logo.png" className="h-8 w-8 mr-5 md:h-12 md:w-12 md:mr-8" alt="Flowbite Logo" width={100} height={100} />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap text-green-700 dark:text-white">Elsa Catoire</span>
                 </Link>
 
                 <div className="w-full md:block md:w-auto" id="navbar-default">
-                    <ul className="font-medium flex p-4 md:p-0 mt-4 rounded-lg bg-gray-200 flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-gray-200 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <ul className="md:font-medium flex p-4 md:p-0 mt-4 rounded-lg bg-gray-200 flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-gray-200 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
                             <Link href="/" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Accueil</Link>
                         </li>
