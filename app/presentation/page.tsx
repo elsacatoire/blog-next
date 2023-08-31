c0bda83a7e9100ee894055fcc91d21acc83702ca
Elsa Catoire <elsa.catoire@gmail.com>
2023-08-31 18:33:53 +0200

    maj présentation

--- app/presentation/page.tsx
+++ app/presentation/page.tsx
@@ -6,10 +6,9 @@ export default function Présentation() {
             <div className="text-base text-left block max-w-m p-6 text-gray-900 bg-gray-200 border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                 <div className="flex flex-wrap mt-5">
                     <div className="md:w-2/3 md:order-1 md:pr-8 m-auto">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">En savoir un petit peu plus sur moi 👋&nbsp;</h5>
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">En savoir un petit peu plus sur moi &#x1F60A; </h5>
                         <br />
-                        <p className="font-normal text-gray-700 dark:text-gray-400"> Je m&rsquo;appelle Elsa, j&rsquo;ai 34 ans, et je suis étudiante en développement
-                            web et logiciel chez Ada Tech School à Nantes,
+                        <p className="font-normal text-gray-700 dark:text-gray-400"> En reconversion professionnelle pour devenir développeuse,
                             {/* je recherche une alternance d&rsquo;un an à partir d&rsquo;octobre ou de novembre 2023,
                             pour travailler 4 jours par semaine. */}</p>
                         <br />
@@ -19,7 +18,7 @@ export default function Présentation() {
                         <br />
                         <p className="font-normal text-gray-700 dark:text-gray-400">Dans mon précédent métier en tant que manager de projets et établissements culturels dans le secteur public, j&rsquo;ai acquis une précieuse expérience dans la prise de responsabilités de manière indépendante. De plus, mes expériences de voyages en solo ont renforcé ma capacité à être fiable et autonome, des qualités que je considère comme essentielles dans n&rsquo;importe quel environnement de travail, même en dehors du cadre technique.</p>
                         <br />
-                        <p className="font-normal text-gray-700 dark:text-gray-400">Toujours passionnée par les problématiques du service public, je me sentais quelque peu limitée par les anciennes techniques de gestion et d&rsquo;organisation de l&rsquo;administration. Découvrir l&rsquo;école Ada Tech School, qui met l&rsquo;accent sur l&rsquo;inclusion dans l&rsquo;industrie technologique, a été un moment décisif pour moi, car cela m&rsquo;a permis d&rsquo;envisager un avenir professionnel dans des projets dynamiques et renouer avec ma passion pour les sciences et la logique. Pouvoir allier mes compétences techniques ainqi que mes qualités d&rsquo;organisation et relationnelles en somme !</p>
+                        <p className="font-normal text-gray-700 dark:text-gray-400">Toujours passionnée par les problématiques du service public, je me sentais quelque peu limitée par les anciennes techniques de gestion et d&rsquo;organisation de l&rsquo;administration. Découvrir l&rsquo;école Ada Tech School, qui met l&rsquo;accent sur l&rsquo;inclusion dans l&rsquo;industrie de la tech, a été un moment décisif pour moi, car cela m&rsquo;a permis d&rsquo;envisager un avenir professionnel dans des projets dynamiques et renouer avec ma passion pour les sciences et la logique. Pouvoir allier compétences techniques et qualités organisationnelles/relationnelles en somme !</p>
                         <br />
                         <p className="font-normal text-gray-700 dark:text-gray-400">Dans le code ce qui me plait le plus : La résolution de problèmes.
                             J&rsquo;ai commencé à apprendre le développement web avec JavaScript et à travailler sur des algorithmes avec Python.
@@ -27,8 +26,8 @@ export default function Présentation() {
                             Ma passion pour ce métier de développeuse réside dans l&rsquo;apprentissage continu.
                             <p>Apprendre quoi ?</p>
                             <p>Tout !</p>
-                            L&rsquo;intérêt c&rsquo;est le produit. Ce qu&rsquo;il me permet de travailler et ce qu&rsquo;il apporte à mes contemporains.
-                            Ce qui compte le plus pour moi, c&rsquo;est l&rsquo;impact final du produit.
+                            L&rsquo;intérêt c&rsquo;est le produit, ce qu&rsquo;il me permet d'apprendre et ce qu&rsquo;il apporte à mes contemporains.
+                            Le plus important pour moi, c&rsquo;est l&rsquo;impact final du produit.
                             Je suis prête à apprendre n&rsquo;importe quelle technologie pour un projet qui m&rsquo;enthousiasme et qui correspond à mes valeurs.</p>
 
 
