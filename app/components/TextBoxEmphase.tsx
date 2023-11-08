866e1872090bfa1085ec0c6470e2857a86f44223
Elsa <elsa.catoire@apside.com>
2023-11-08 10:07:20 +0100

    refactor textBox homepage

---
+++ app/components/TextBoxEmphase.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type TextBoxProps = {
+    title: string;
+    items: string[];
+};
+
+const TextBoxEmphase = ({ title, items }: TextBoxProps) => {
+    return (
+        <div className="w-full md:w-1/3 md:order-1 p-3">
+            <div className="h-full max-w-sm p-6 bg-gray-300 border mx-auto border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+                {items.map((item, index) => (
+                    <p key={index} className="font-normal text-gray-700 dark:text-gray-400">
+                        {item}
+                    </p>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default TextBoxEmphase;
