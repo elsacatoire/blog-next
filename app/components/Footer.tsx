44e89e3f1686706788cfb656c1d2886165b5f3d9
Elsa Catoire <elsa.catoire@gmail.com>
2024-10-13 12:07:38 +0200

    add biome and refactor Footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,36 +2,30 @@ import Link from "next/link";
 import { FaGithub, FaLinkedin, FaMedium, FaInstagram } from "react-icons/fa"
 
 export default function Footer() {
-    return (
-        <footer className="bg-gray-200 shadow dark:bg-gray-800">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <span className="text-sm text-green-700 sm:text-left dark:text-gray-400">© 2023.
-                    <Link href="/" className="hover:underline">Elsa Catoire</Link>. All Rights Reserved.
-                    <p>Crafted with Next.js, TailwindCSS, and Markdown, using my own hands</p>
-                </span>
-                <ul className="flex flex-wrap items-center mt-8 text-sm font-medium text-green-700 dark:text-gray-400 sm:mt-0">
-                    <li>
-                        <Link href="https://www.linkedin.com/in/elsa-catoire-11402753/" className="mr-5 hover:underline md:mr-10" target="_blank" rel="noopener noreferrer">
-                            <FaLinkedin className="w-10 h-10 m-2" />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="https://github.com/elsacatoire" className="mr-5 hover:underline md:mr-10" target="_blank" rel="noopener noreferrer">
-                            <FaGithub className="w-10 h-10 m-2" />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="https://www.instagram.com/elsa_des_bois/" className="mr-5 hover:underline md:mr-10" target="_blank" rel="noopener noreferrer">
-                            <FaInstagram className="w-10 h-10 m-2" />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="https://medium.com/@elsa.catoire" className="mr-5 hover:underline md:mr-10" target="_blank" rel="noopener noreferrer">
-                            <FaMedium className="w-10 h-10 m-2" />
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-        </footer>
-    )
+	const links = [
+		{ href: "https://www.linkedin.com/in/elsa-catoire-11402753/", icon: FaLinkedin },
+		{ href: "https://github.com/elsacatoire", icon: FaGithub },
+		{ href: "https://www.instagram.com/elsa_des_bois/", icon: FaInstagram },
+		{ href: "https://medium.com/@elsa.catoire", icon: FaMedium }
+	];
+
+	return (
+		<footer className="bg-gray-200 shadow dark:bg-gray-800">
+			<div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+				<span className="text-sm text-green-700 sm:text-left dark:text-gray-400">© 2023.
+					<Link href="/" className="hover:underline">Elsa Catoire</Link>. All Rights Reserved.
+					<p>Crafted with Next.js, TailwindCSS, and Markdown, using my own hands</p>
+				</span>
+				<ul className="flex flex-wrap items-center mt-8 text-sm font-medium text-green-700 dark:text-gray-400 sm:mt-0">
+					{links.map((link, index) => (
+						<li key={index} className="flex gap-1">
+							<Link href={link.href} className="mr-5 hover:underline md:mr-10" target="_blank" rel="noopener noreferrer">
+								<link.icon className="w-10 h-10" />
+							</Link>
+						</li>
+					))}
+				</ul>
+			</div>
+		</footer>
+	)
 }
--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link"
 import getFormattedDate from "@/lib/getFormattedDate"
-import Image from "next/image"
 
 type Props = {
     post: BlogPost
@@ -14,7 +13,7 @@ export default function ListItem({ post }: Props) {
         <Link className="mt-5 mx-auto w-full flex flex-col items-center rounded-lg bg-gray-300 border shadow md:flex-row md:max-w-xl  dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700" href={`/posts/${id}`}>
             <img className="imgProject rounded-l" src={image} alt="" width={100} height={100} />
             <div className="flex flex-col justify-between p-4 leading-normal">
-                <h5 className="mb-2 text-1xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+                <h1 className="mb-2 text-1xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h1>
                 <p className="text-left mt-7 text-sm font-normal text-green-700 dark:text-gray-400">{formattedDate}</p>
             </div>
         </Link>
