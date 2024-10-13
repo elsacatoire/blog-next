0d8ee73edaefaacf3e492c8a2bc32fd49a726791
Elsa Catoire <elsa.catoire@gmail.com>
2024-10-13 12:09:48 +0200

    refactor menu

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
 
+const navbarItems = [
+    { href: "/", label: "Accueil" },
+    { href: "/presentation", label: "About" },
+    { href: "/posts", label: "Blog" },
+    { href: "/projets", label: "Projets" },
+    { href: "/voyages", label: "Voyages" },
+    { href: "/contact", label: "Contact" }
+]
+
 export default function Navbar() {
     return (
         <nav className="bg-gray-200 border-gray-200 dark:bg-gray-900 md:p-2 sticky top-0 drop-shadow-xl z-10">
@@ -12,24 +21,13 @@ export default function Navbar() {
 
                 <div className="w-full md:block md:w-auto" id="navbar-default">
                     <ul className="md:font-medium flex md:p-4 mt-4 rounded-lg bg-gray-200 flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-gray-200 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                        <li>
-                            <Link href="/" className="hidden md:block md:py-2 sm:pl-1 md:pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Accueil</Link>
-                        </li>
-                        <li>
-                            <Link href="/presentation" className="block md:py-2 md:pl-2 px-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</Link>
-                        </li>
-                        <li>
-                            <Link href="/posts" className="block md:py-2 md:pl-3 px-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blog</Link>
-                        </li>
-                        <li>
-                            <Link href="/projets" className="block md:py-2 md:pl-3 px-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Projets</Link>
-                        </li>
-                        <li>
-                            <Link href="/voyages" className="block md:py-2 md:pl-3 px-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Voyages</Link>
-                        </li>
-                        <li>
-                            <Link href="/contact" className="block md:py-2 md:pl-3 px-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</Link>
-                        </li>
+                        {navbarItems.map((item) => (
+                            <li key={item.href}>
+                                <Link href={item.href} className="block md:py-2 md:pl-3 px-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                                    {item.label}
+                                </Link>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
