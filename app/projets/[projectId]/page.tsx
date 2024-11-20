a72ec36d1c6d3e431220d01d792d0f8c473c1533
Elsa <elsa.catoire@apside.com>
2024-11-20 16:52:14 +0100

    fix: bump to next 14 with fixes

--- app/projets/[projectId]/page.tsx
+++ app/projets/[projectId]/page.tsx
@@ -3,47 +3,50 @@ import { getProjectData, getSortedProjectsData } from "@/lib/projects";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
-export function generateMetadata({
-	params,
-}: { params: { projectId: string } }) {
-	const projects = getSortedProjectsData();
-	const { projectId } = params;
+export function generateMetadata({ params }: { params: { projectId: string } }) {
+    const projects = getSortedProjectsData();
+    const { projectId } = params;
 
-	const project = projects.find((project) => project.id === projectId);
+    const project = projects.find((project) => project.id === projectId);
 
-	if (!project) {
-		return {
-			title: "Post Not Found",
-		};
-	}
+    if (!project) {
+        return {
+            title: "Post Not Found",
+        };
+    }
 
-	return {
-		title: project.title,
-	};
+    return {
+        title: project.title,
+    };
 }
 
-export default async function Project({
-	params,
-}: { params: { projectId: string } }) {
-	const projects = getSortedProjectsData();
-	const { projectId } = params;
+export function generateStaticParams() {
+    const projects = getSortedProjectsData();
+    return projects.map((project) => ({
+        projectId: project.id,
+    }));
+}
 
-	if (!projects.find((project) => project.id === projectId)) notFound();
+export default async function Project({ params }: { params: { projectId: string } }) {
+    const projects = getSortedProjectsData();
+    const { projectId } = params;
 
-	const { title, date, contentHtml } = await getProjectData(projectId);
+    if (!projects.find((project) => project.id === projectId)) notFound();
 
-	const pubDate = getFormattedDate(date);
+    const { title, date, contentHtml } = await getProjectData(projectId);
 
-	return (
-		<div className="mx-auto mb-10 mt-10 max-w-4xl text-gray-900 bg-gray-200 border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-			<main className="markdown-content px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
-				<h1 className="text-3xl mt-4 mb-0">{title}</h1>
-				<p className="mt-0">{pubDate}</p>
-				<article dangerouslySetInnerHTML={{ __html: contentHtml }} />
-				<p className="mb-8 mt-8 text-xl text-green-700 font-semibold">
-					<Link href="/projets">← Retour aux projets</Link>
-				</p>
-			</main>
-		</div>
-	);
+    const pubDate = getFormattedDate(date);
+
+    return (
+        <div className="mx-auto mb-10 mt-10 max-w-4xl text-gray-900 bg-gray-200 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <main className="markdown-content px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+                <h1 className="text-3xl mt-4 mb-0">{title}</h1>
+                <p className="mt-0">{pubDate}</p>
+                <article dangerouslySetInnerHTML={{ __html: contentHtml }} />
+                <p className="mb-8 mt-8 text-xl text-green-700 font-semibold">
+                    <Link href="/projets">← Retour aux projets</Link>
+                </p>
+            </main>
+        </div>
+    );
 }
