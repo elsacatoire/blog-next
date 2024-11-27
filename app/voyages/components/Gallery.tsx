252cddbcadedf8570dae22b3b33af0d959e49075
Elsa Catoire <123973199+elsacatoire@users.noreply.github.com>
2024-11-27 16:42:28 +0100

    refacto voyages avec ajout ouverture des images dans la gallerie (#39)
    Co-authored-by: Elsa <elsa.catoire@apside.com>

--- app/voyages/components/Gallery.tsx
+++ app/voyages/components/Gallery.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 const images = [
 	{ src: "/images/hike1.jpg", alt: "Hiking in the mountains" },
@@ -13,19 +15,59 @@ const images = [
 ];
 
 const Gallery = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [currentImage, setCurrentImage] = useState<string | null>(null);
+
+	const handleImageClick = (src: string) => {
+		setCurrentImage(src);
+		setIsOpen(true);
+	};
+
+	const handleClose = () => {
+		setIsOpen(false);
+		setCurrentImage(null);
+	};
+
 	return (
-		<div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-			{images.map((image) => (
-				<div key={image.src} className="aspect-ratio-1-1">
-					<Image
-						className="object-cover w-full h-full rounded-lg"
-						src={image.src}
-						alt={`Image ${image.alt}`}
-						width={100}
-						height={100}
-					/>
+		<div>
+			<div className="grid grid-cols-2 md:grid-cols-4 gap-2">
+				{images.map((image) => (
+					<div key={image.src} className="aspect-square cursor-pointer">
+						<Image
+							className="object-cover w-full h-full rounded-lg"
+							src={image.src}
+							alt={image.alt}
+							width={400}
+							height={400}
+							onClick={() => handleImageClick(image.src)}
+							priority={true}
+						/>
+					</div>
+				))}
+			</div>
+
+			{isOpen && currentImage && (
+				<div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80">
+					<div className="relative max-w-full max-h-screen">
+						<Image
+							src={currentImage}
+							alt="Image agrandie"
+							width={800}
+							height={800}
+							className="object-contain rounded-lg max-w-full max-h-screen"
+							style={{ width: "auto", height: "auto" }}
+							priority={true}
+						/>
+						<button
+							type="button"
+							onClick={handleClose}
+							className="absolute top-2 right-2 bg-white text-black rounded-full px-4 py-2"
+						>
+							X
+						</button>
+					</div>
 				</div>
-			))}
+			)}
 		</div>
 	);
 };
--- app/voyages/page.tsx
+++ app/voyages/page.tsx
@@ -1,131 +1,87 @@
 import Image from "next/image";
 import Link from "next/link";
+import { v4 as uuidv4 } from "uuid";
+import CustomCardList from "../components/CustomCardList";
 import Gallery from "./components/Gallery";
+
+const productionCardContent = {
+	title: "Production",
+	items: [
+		"57 000km parcourus",
+		"22 pays",
+		"9 woofings",
+		"Pair & mob voyages",
+		"Création et animation de communautés de voyageurs libres",
+		"Beaucoup de riens",
+	],
+};
+
+const stackCardContent = {
+	title: "Stack",
+	items: [
+		"Renault Trafic L1L2",
+		"Batterie Lithium 100A",
+		"60L eau propre",
+		"Douche solaire",
+		"Pelle",
+		"Tout réparer avec du gaffeur",
+		"Park4Night",
+		"Chaussettes pilou",
+	],
+};
+
+const skillsCardContent = {
+	title: "Skills",
+	items: [
+		"Autonomie",
+		"Langages : anglais fluent, roumain, serbo-croate, turc",
+		"Ukulélé",
+		"Conduire un tracteur",
+		"Traite et fabrication de fromage",
+		"Randonnée",
+	],
+};
 
 export default function Voyages() {
 	return (
-		<>
-			<div className="">
-				<div className="flex flex-col-reverse md:flex-row md:space-x-8 items-start">
-					<div className="md:mt-5 md:w-1/3 md:order-1 ml-5">
-						<h5 className="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-							Pérégrinations
-						</h5>
-						<br />
-						<p className="text-center font-semibold text-gray-700 dark:text-gray-400">
-							[Octobre 2020 - Janvier 2023]
-						</p>
-						<p className="font-normal text-gray-700 dark:text-gray-400">
-							Plus de 2 ans de voyages et bénévolats sur les routes
-							d&rsquo;Europe. Du Portugual à la Turquie en passant par
-							l&rsquo;Albanie et en rentrant par la Slovaquie... Un voyage
-							immersif, en solo, autonome et libre.
-						</p>
-						<br />
-					</div>
-					<div className="md:w-2/3 md:order-2 rounded-lg center">
-						<Link href="https://www.polarsteps.com/ElsaCatoire">
-							<Image
-								className="mx-auto mt-0 rounded-lg"
-								src="/images/polarstep.jpg"
-								width={1000}
-								height={1000}
-								alt="Elsa Catoire"
-								priority={true}
-							/>
-						</Link>
-					</div>
+		<div className="flex flex-col gap-4">
+			<div className="flex flex-col-reverse md:flex-row md:space-x-8 items-start">
+				<div className="flex flex-col gap-4 md:mt-5 md:w-1/3 md:order-1 ml-5">
+					<h1 className="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+						Pérégrinations
+					</h1>
+					<p className="text-center font-semibold text-gray-700 dark:text-gray-400">
+						[Octobre 2020 - Janvier 2023]
+					</p>
+					<p className="font-normal text-gray-700 dark:text-gray-400">
+						Plus de 2 ans de voyages et bénévolats sur les routes
+						d&rsquo;Europe. Du Portugual à la Turquie en passant par
+						l&rsquo;Albanie et en rentrant par la Slovaquie... Un voyage
+						immersif, en solo, autonome et libre.
+					</p>
 				</div>
-				<div className="justify-center flex flex-wrap mt-12">
-					<div className="w-full md:w-1/3 md:order-1 p-3">
-						<div className="h-full max-w-sm p-6 bg-gray-300 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-							<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-								Production
-							</h5>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								&#x2022; 57 000km parcourus
-							</p>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								&#x2022; 22 pays
-							</p>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								&#x2022; 9 woofings
-							</p>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								&#x2022; Pair & mob voyages
-							</p>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								&#x2022; Création et alimentation de communautés de voyageurs
-								libres
-							</p>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								&#x2022; Beaucoup de riens
-							</p>
-						</div>
-					</div>
-					<div className="w-full md:w-1/3 md:order-2 p-3">
-						<div className="h-full max-w-sm p-6 bg-gray-300 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-							<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-								Stack
-							</h5>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								&#x2022; Renault Trafic L1L2
-							</p>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								&#x2022; Batterie Lithium 100A
-							</p>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								&#x2022; 60L eau propre
-							</p>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								&#x2022; Douche solaire
-							</p>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								&#x2022; Pelle
-							</p>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								&#x2022; Reine du gaffeur
-							</p>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								&#x2022; Park4Night
-							</p>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								&#x2022; Chaussettes pilou
-							</p>
-						</div>
-					</div>
-
-					<div className="w-full md:w-1/3 md:order-3 p-3">
-						<div className="h-full max-w-sm p-6 bg-gray-300 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-							<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-								Skills
-							</h5>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								&#x2022; Autonomie
-							</p>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								&#x2022; Langages : anglais fluent, roumain, serbo-croate, turc
-							</p>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								&#x2022; Ukulélé
-							</p>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								&#x2022; Conduire un tracteur
-							</p>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								&#x2022; Traite et fabrication de fromage
-							</p>
-							<p className="font-normal text-gray-700 dark:text-gray-400">
-								&#x2022; Randonnée
-							</p>
-						</div>
-					</div>
+				<div className="md:w-2/3 md:order-2 rounded-lg center">
+					<Link href="https://www.polarsteps.com/ElsaCatoire">
+						<Image
+							className="mx-auto mt-0 rounded-lg"
+							src="/images/polarstep.jpg"
+							width={800}
+							height={800}
+							alt="Elsa Catoire"
+						/>
+					</Link>
 				</div>
 			</div>
-
-			<div>
-				<Gallery />
+			<div className="flex flex-wrap gap-6 justify-between">
+				{[productionCardContent, stackCardContent, skillsCardContent].map(
+					(content) => (
+						<div key={uuidv4()} className="w-full md:w-[30%]">
+							<CustomCardList title={content.title} items={content.items} />
+						</div>
+					),
+				)}
 			</div>
-		</>
+			<Gallery />
+		</div>
 	);
 }
